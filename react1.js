f01de264ce4c0756d795612c9506d08de53594d3
Karim Toulba <karim@wadipurple.com>
2024-07-28 23:39:55 +0300

    Update react1.js

--- react1.js
+++ react1.js
@@ -3,3 +3,8 @@
 const var = <h1>Hello World</h1>;
 const p1 = <p id="large">foo</p>;
 const myDiv = (<div><h1>Hello World</h1></div>);
+
+// Rendering JSX Elements
+const container = document.getELementById('app');
+const root = createRoot(container);
+root.render(<h1>Hello World</h1>);
